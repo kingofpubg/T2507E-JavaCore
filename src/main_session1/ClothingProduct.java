124f50ac6639022c355f757efca348f72d61e43e
kingofpubg <giahuy04041999@gmail.com>
2026-01-28 17:59:50 +0700

    Add ASM3 bai 1 - Product hierarchy

--- src/main_session1/ClothingProduct.java
+++ src/main_session1/ClothingProduct.java
@@ -15,4 +15,17 @@ public class ClothingProduct extends Product {
     public double calculateDiscountedPrice() {
         return getPrice() * 0.85; // giảm 15%
     }
+
+    // ===== MAIN PHẢI NẰM TRONG CLASS =====
+    public static void main(String[] args) {
+        ClothingProduct cp = new ClothingProduct(
+                "C01",
+                "Áo thun",
+                200000,
+                "L"
+        );
+
+        System.out.println(cp.getInfo());
+        System.out.println("Giá sau giảm: " + cp.calculateDiscountedPrice());
+    }
 }
